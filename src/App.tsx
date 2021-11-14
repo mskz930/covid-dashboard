6863e4b545b4dca06b462dac0840aa33b4cd129f
mskz930 <m.kappa930@gmail.com>
2021-11-14 19:51:08 +0900

    sss

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Cards from './features/covid/Cards/Cards';
+import Chart from './features/covid/Chart/Chart';
+import PieChart from './features/covid/PieChart/PieChart';
+import SwitchCountry from './features/covid/SwitchCountry/SwitchCountry';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Cards />
+        <Chart />
+        <PieChart />
+        <SwitchCountry />
       </header>
     </div>
   );
---
+++ src/features/covid/DashBoard/DashBoard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const DashBoard: React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DashBoard
